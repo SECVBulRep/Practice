39947d95b251ac856c642d86c61cc0b1f8551dfa
secvbulrep <bfarukshin@gmail.com>
2021-10-06 09:13:51 +0300

    init

---
+++ EF.PG/EF.PG/ConsoleApp/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        //public DateTime Stamp { get; set; }
+    }
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=WMDATASTORE;Port=5432;Database=Test;Username=bulat;Password=p@ssw0rd.");
+        }
+    }
+}
---
+++ asyncawait/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("main start " + Thread.CurrentThread.ManagedThreadId);
+
+
+            try
+            {
+                var t1 = new Task(DoWork);
+                await t1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+         
+            
+
+            Console.WriteLine("main end " + Thread.CurrentThread.ManagedThreadId);
+            Console.ReadKey();
+        }
+
+   
+
+
+        public static async void DoWork()
+        {
+            Console.WriteLine("DoWork start " + Thread.CurrentThread.ManagedThreadId);
+
+            await Task.Run(() => { Thread.Sleep(1000); });
+
+            throw new Exception();
+            
+            Console.WriteLine("DoWork end " + Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
