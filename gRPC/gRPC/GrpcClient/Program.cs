994208446a858baa7f8f54c5c8e3fc9312e99ac1
SECVBulRep <bfarukshin@gmail.com>
2021-10-10 19:51:53 +0300

    рассмотрел все примеры общзения по grpc

--- gRPC/gRPC/GrpcClient/Program.cs
+++ gRPC/gRPC/GrpcClient/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using Grpc.Core;
 using Grpc.Net.Client;
 
 namespace GrpcClient
@@ -9,18 +11,71 @@ namespace GrpcClient
         static async Task Main(string[] args)
         {
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-
             var client = new Greeter.GreeterClient(channel);
+        
 
-            var reply = await client.SayHelloAsync(
-                new HelloRequest { Name = "GreeterClient" });
+            // унарный вызов
+            var unarReply = await client.UnaryCallAsync(new ExampleRequest { RequestId = Guid.NewGuid().ToString() });
+            Console.WriteLine("unarReply: " + unarReply.RequestId);
 
-            var reply2 = await client.SayHello2Async(new HelloRequest { Name = "GreeterClient" });
+            // передача данных их сервера 
+            using (var streamingFromServerReplays = client.StreamingFromServer(new ExampleRequest { RequestId = Guid.NewGuid().ToString() }))
+            {
+                var i = 0;
+                while (await streamingFromServerReplays.ResponseStream.MoveNext(CancellationToken.None))
+                {
+                    Console.WriteLine("StreamingFromServer: " + streamingFromServerReplays.ResponseStream.Current.RequestId);
+                    i++;
+                }
+            }
 
-            Console.WriteLine("Greeting: " + reply.Message);
-            Console.WriteLine("Greeting: " + reply2.Message);
+            //пеоедача данных их клиента
+            AsyncClientStreamingCall<ExampleRequest, ExampleResponse> call =  client.StreamingFromClient();
+            for (var i = 0; i < 3; i++)
+            {
+                await call.RequestStream.WriteAsync(new ExampleRequest { RequestId = Guid.NewGuid().ToString() });
+            }
+            await call.RequestStream.CompleteAsync();
+            var streamingFromClientreplay = await call;
+            
+            Console.WriteLine("StreamingFromClient: " + streamingFromClientreplay.RequestId);
+            
+   
+            // двухсторонняя передача данных
+            using var StreamingBothWayscall = client.StreamingBothWays();
+
+            Console.WriteLine("Starting background task to receive messages");
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (var response in StreamingBothWayscall.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine(response.RequestId);
+                    // Echo messages sent to the service
+                }
+            });
+
+            Console.WriteLine("Starting to send messages");
+            Console.WriteLine("Type a message to echo then press enter.");
+            while (true)
+            {
+                var result = Console.ReadLine();
+                if (string.IsNullOrEmpty(result))
+                {
+                    break;
+                }
+
+                await StreamingBothWayscall.RequestStream.WriteAsync(new ExampleRequest{RequestId = result});
+            }
+
+            Console.WriteLine("Disconnecting");
+            await StreamingBothWayscall.RequestStream.CompleteAsync();
+            await readTask;
+
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
+
+      
+
     }
 }
--- gRPC/gRPC/GrpcService/Services/GreeterService.cs
+++ gRPC/gRPC/GrpcService/Services/GreeterService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
@@ -24,7 +26,6 @@ namespace GrpcService.Services
 
         public override Task<HelloReply> SayHello2(HelloRequest request, ServerCallContext context)
         {
-            
             _logger.LogWarning(request.Name);
             
             return Task.FromResult(new HelloReply
@@ -32,5 +33,73 @@ namespace GrpcService.Services
                 Message = "Hello2 " + request.Name
             });
         }
+        
+        public override Task<ExampleResponse> UnaryCall(ExampleRequest request,
+            ServerCallContext context)
+        {
+            _logger.LogWarning("UnaryCall "+ request.RequestId);
+            
+            var response = new ExampleResponse();
+            response.RequestId = request.RequestId + " " + DateTime.Now.ToString();
+            return Task.FromResult(response);
+        }
+        
+        public override async Task StreamingFromServer(ExampleRequest request,
+            IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            
+            _logger.LogWarning("StreamingFromServer "+ request.RequestId);
+            
+            for (var i = 0; i < 3; i++)
+            {
+                await responseStream.WriteAsync(new ExampleResponse{RequestId = "StreamingFromServer response  "+i+" "+DateTime.Now+ request.RequestId});
+                _logger.LogWarning("StreamingFromServer response  "+i+" "+DateTime.Now+ request.RequestId);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+        
+        public override async Task<ExampleResponse> StreamingFromClient(
+            IAsyncStreamReader<ExampleRequest> requestStream, ServerCallContext context)
+        {
+            string allReqs = "";
+            
+            while (await requestStream.MoveNext())
+            {
+                var message = requestStream.Current;
+                allReqs = allReqs +" "+ message.RequestId+" "+DateTime.Now+" "+Environment.NewLine;
+            }
+            
+            _logger.LogWarning("StreamingFromClient "+ allReqs);
+            return new ExampleResponse{RequestId = allReqs};
+        }
+        
+        
+        ConcurrentBag<string> cb = new ConcurrentBag<string>();
+        
+        public override async Task StreamingBothWays(IAsyncStreamReader<ExampleRequest> requestStream,
+            IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            // Read requests in a background task.
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (var message in requestStream.ReadAllAsync())
+                {
+                    _logger.LogWarning("StreamingBothWays MessageFromClient "+ message.RequestId);
+                    cb.Add(message.RequestId);
+                    // Process request.
+                }
+            });
+    
+            // Send responses until the client signals that it is complete.
+            while (!readTask.IsCompleted)
+            {
+                string res;
+                if(cb.TryTake(out res)){
+                    await responseStream.WriteAsync(new ExampleResponse{RequestId = res});
+                }
+                await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
+            }
+        }
+        
     }
 }
