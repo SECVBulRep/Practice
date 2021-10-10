69c7f6a9b849819a5a09a43b5edd5034bfb941d9
SECVBulRep <bfarukshin@gmail.com>
2021-10-10 18:29:17 +0300

    fix

---
+++ gRPC/gRPC/GrpcClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" });
+
+            var reply2 = await client.SayHello2Async(new HelloRequest { Name = "GreeterClient" });
+
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Greeting: " + reply2.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ gRPC/gRPC/GrpcService/Services/GreeterService.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            _logger.LogWarning(request.Name);
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+
+        public override Task<HelloReply> SayHello2(HelloRequest request, ServerCallContext context)
+        {
+            
+            _logger.LogWarning(request.Name);
+            
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello2 " + request.Name
+            });
+        }
+    }
+}
