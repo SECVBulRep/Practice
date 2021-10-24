427a22b3afb1fb0ab5d21dba1b2a4956191f82cd
SECVBulRep <bfarukshin@gmail.com>
2021-10-24 20:58:52 +0300

    добавил

---
+++ gRPC/gRPC/GrpcClientFactory/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace GrpcClientFactory
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args)
+                    .ConfigureAppConfiguration((builderContext, config) =>
+                    {
+                    })
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        services.InitQuartz(hostContext.Configuration,hostContext.HostingEnvironment);
+                        
+                        services.AddGrpcClient<Greeter.GreeterClient>(o =>
+                        {
+                            o.Address = new Uri("https://localhost:5001");
+                        }); 
+                    })
+                    .ConfigureLogging((context, builder) => { });
+                ;
+
+            await builder.RunConsoleAsync();
+        }
+    }
+
+   
+    
+    [DisallowConcurrentExecution]
+    public class TestJob : IJob
+    {
+        private Greeter.GreeterClient _greeterClient;
+        private readonly ILogger<TestJob> _logger;
+      
+        public TestJob(ILogger<TestJob> logger, Greeter.GreeterClient greeterClient)
+        {
+            _logger = logger;
+            _greeterClient = greeterClient;
+        }
+
+        public async Task Execute(IJobExecutionContext context) {
+            try
+            {
+                var response = await _greeterClient.UnaryCallAsync(new ExampleRequest { RequestId = "sdfsdf"});
+
+                if (response != null) _logger.LogInformation("from grpc - " + response.RequestId);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+            }
+        }
+    }
+    
+    
+}
---
+++ gRPC/gRPC/GrpcClientFactory/QuartzHelpers.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace GrpcClientFactory
+{
+    public static class QuartzServices
+    {
+        public static void InitQuartz(this IServiceCollection services, IConfiguration configuration,
+            IHostEnvironment environment)
+        {
+            services.Configure<QuartzOptions>(configuration.GetSection("Quartz"));
+
+            var options = new QuartzOptions();
+            configuration.Bind("Quartz", options);
+            services.AddQuartz(q =>
+            {
+                q.UseMicrosoftDependencyInjectionScopedJobFactory();
+ 
+                if (environment.IsDevelopment())
+                {
+                    q.SchedulerName = "Ait.App.Background.Test." + Environment.MachineName;
+                }
+                q.AddJobAndTrigger<TestJob>(configuration);
+            });
+            services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+        }
+    }
+    
+     public static class ServiceCollectionQuartzConfiguratorExtensions
+    {
+        public static void AddJobAndTrigger<T>(
+            this IServiceCollectionQuartzConfigurator quartz,
+            IConfiguration config)
+            where T : IJob
+        {
+            string jobName;
+            string schedName;
+
+            if (typeof(T).BaseType?.Name == nameof(QuartzJob))
+            {
+                jobName = $"QuartzJob<{typeof(T).GetGenericArguments().First().FullName}>";
+                schedName = typeof(T).GetGenericArguments().First().Name;
+            }
+            else
+            {
+                jobName = typeof(T).Name;
+                schedName = jobName;
+            }
+
+            // Try and load the schedule from configuration
+            var configKey = $"Jobs:{schedName}";
+            var cronSchedule = config[configKey];
+
+            // Some minor validation
+            if (string.IsNullOrEmpty(cronSchedule))
+            {
+                return;
+            }
+
+            // register the job as before
+            var jobKey = new JobKey(jobName);
+            quartz.AddJob<T>(opts => opts.WithIdentity(jobKey));
+
+            quartz.AddTrigger(opts => opts
+                .ForJob(jobKey)
+                .WithIdentity(jobName + ".trigger")
+                .WithCronSchedule(cronSchedule)); // use the schedule from configuration
+        }
+    }
+    
+    public abstract class QuartzJob : IJob, IDisposable
+    {
+        protected readonly IServiceScope Scope;
+        protected readonly ILogger<QuartzJob> Logger;
+
+        protected QuartzJob(IServiceProvider serviceProvider, ILogger<QuartzJob> logger)
+        {
+            Logger = logger;
+            Scope = serviceProvider.CreateScope();
+        }
+
+        public abstract Task Execute(IJobExecutionContext context);
+
+        public void Dispose()
+        {
+            Scope?.Dispose();
+        }
+    }
+
+    [DisallowConcurrentExecution]
+    public class QuartzJob<T> : QuartzJob
+        where T : IExecutable
+    {
+        public QuartzJob(IServiceProvider serviceProvider, ILogger<QuartzJob> logger)
+            : base(serviceProvider, logger)
+        {
+        }
+
+        public override async Task Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                await Scope.ServiceProvider.GetService<T>().Execute(context.MergedJobDataMap.WrappedMap);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, e.Message);
+            }
+        }
+    }
+    
+    public interface IExecutable
+    {
+        Task Execute(IDictionary<string, object> parameters);
+    }
+}
