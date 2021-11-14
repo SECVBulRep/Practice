ba22ea86d6f9f7b41b7c50becbde4d162c1b093c
SECVBulRep <bfarukshin@gmail.com>
2021-11-14 17:46:46 +0300

    добавил  проект с рабиттом
    добавил  проект с рабиттом

---
+++ RBTMQ/RB.Project/RB.Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace RB.Publisher
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                sbc.Host("localhost", "/", h =>
+                {
+                    h.Username("rabbitmq");
+                    h.Password("rabbitmq");
+                });
+
+
+                sbc.ReceiveEndpoint("test_queue", ep =>
+                {
+                    ep.Handler<Message>(context =>
+                    {
+                        return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                    });
+                });
+            });
+
+            await bus.StartAsync(); // This is important!
+
+            await bus.Publish(new Message { Text = "Hi" });
+
+            Console.WriteLine("Press any key to exit");
+            await Task.Run(() => Console.ReadKey());
+
+            await bus.StopAsync();
+        }
+    }
+
+    public class Message
+    {
+        public string Text { get; set; }
+    }
+
+}
---
+++ RBTMQ/RB.Project/RB.PublisherConsole/Program.cs
@@ -0,0 +1,77 @@
+﻿using MassTransit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RB.PublisherConsole
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host("localhost", "/", h =>
+                {
+                    h.Username("rabbitmq");
+                    h.Password("rabbitmq");
+                });
+
+                cfg.ReceiveEndpoint("event-listener", e =>
+                {
+                    e.Consumer<EventConsumer>();
+                });
+
+                cfg.ReceiveEndpoint("event-listener2", e =>
+                {
+                    e.Consumer<EventConsumer>();
+                });
+
+            });
+
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            await busControl.StartAsync(source.Token);
+            try
+            {
+                do
+                {
+                    string value = await Task.Run(() =>
+                    {
+                        Console.WriteLine("Enter message (or quit to exit)");
+                        Console.Write("> ");
+                        return Console.ReadLine();
+                    });
+
+                    if ("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+                        break;
+
+                    await busControl.Publish<ValueEntered>(new
+                    {
+                        Value = value
+                    });
+                }
+                while (true);
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+
+
+    class EventConsumer :
+           IConsumer<ValueEntered>
+    {
+        public async Task Consume(ConsumeContext<ValueEntered> context)
+        {
+            Console.WriteLine("Value: {0}", context.Message.Value);
+        }
+    }
+
+    public interface ValueEntered
+    {
+        string Value { get; }
+    }
+}
