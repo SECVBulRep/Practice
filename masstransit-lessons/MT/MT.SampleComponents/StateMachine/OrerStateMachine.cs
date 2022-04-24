c4694ea7d28654a31099f8d41cac25d8f717014f
SECVBulRep <bfarukshin@gmail.com>
2022-04-24 21:24:04 +0300

    Init Saga+Reddis Repo

---
+++ masstransit-lessons/MT/MT.SampleComponents/StateMachine/OrerStateMachine.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Mail;
+using MassTransit;
+using MT.SampleContracts;
+
+namespace MT.SampleComponents.StateMachine;
+
+public class OrerStateMachine : MassTransitStateMachine<OrderState>
+{
+    public OrerStateMachine()
+    {
+        Event(() => OrderSubmitted, x => x.CorrelateById(m => m.Message.OrderId));
+
+        InstanceState(x => x.CurrentState);
+
+        Initially(
+            When(OrderSubmitted)
+                .TransitionTo(Submitted)
+        );
+    }
+
+    public State Submitted { get; set; }
+    public Event<IOrderSubmitted> OrderSubmitted { set; get; }
+}
+
+public class OrderState : SagaStateMachineInstance, ISagaVersion
+{
+    public Guid CorrelationId { get; set; }
+    public string CurrentState { get; set; }
+    public int Version { get; set; }
+}
---
+++ masstransit-lessons/MT/MT.SampleContracts/IOrderSubmitted.cs
@@ -0,0 +1,8 @@
+﻿namespace MT.SampleContracts;
+
+public interface IOrderSubmitted
+{
+    Guid OrderId { get; set; }
+    DateTime TimeStamp { get; set; }
+    string CustomerNumber { get; set; }
+}
