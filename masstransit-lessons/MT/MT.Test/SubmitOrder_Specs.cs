f77dbaedd2e1cb7441112c4b3cac8ba7598908d9
SECVBulRep <bfarukshin@gmail.com>
2022-05-09 17:57:48 +0300

    Добавил тесты на разные виды  запросов

--- masstransit-lessons/MT/MT.Test/SubmitOrder_Specs.cs
+++ masstransit-lessons/MT/MT.Test/SubmitOrder_Specs.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 public class When_an_order_request_is_consumed
 {
     [Test]
-    public async Task Shuold_response_with_acceptance_if_ok()
+    public async Task Should_response_with_acceptance_if_ok()
     {
         var harness = new InMemoryTestHarness();
         var consumer = harness.Consumer<SubmitOrderConsumer>();
@@ -28,6 +28,73 @@ public class When_an_order_request_is_consumed
             });
 
             Assert.That(consumer.Consumed.Select<ISubmitOrder>().Any(), Is.True);
+
+        }
+        finally
+        {
+            await harness.Stop();
+        }
+    }
+    
+    [Test]
+    public async Task Should_response_with_acceptance_if_ok2()
+    {
+        var harness = new InMemoryTestHarness();
+        var consumer = harness.Consumer<SubmitOrderConsumer>();
+
+        await harness.Start();
+        try
+        {
+
+            var orderId = Guid.NewGuid();
+            var stamp = DateTime.UtcNow;
+            var customerId = "Bob";
+
+            var request_client = await harness.ConnectRequestClient<ISubmitOrder>();
+            
+            var response = await request_client.GetResponse<IOrderSubmissionAccepted>(new
+            {
+                OrderId = orderId,
+                TimeStamp = stamp,
+                CustomerNumber = customerId
+            });
+
+            Assert.That(consumer.Consumed.Select<ISubmitOrder>().Any(), Is.True);
+            Assert.That(response.Message.OrderId, Is.EqualTo(orderId));
+            Assert.That(harness.Sent.Select<IOrderSubmissionAccepted>().Any,Is.True);
+
+        }
+        finally
+        {
+            await harness.Stop();
+        }
+    }
+    
+    public async Task Should_response_with_rejected_if_ok2()
+    {
+        var harness = new InMemoryTestHarness();
+        var consumer = harness.Consumer<SubmitOrderConsumer>();
+
+        await harness.Start();
+        try
+        {
+
+            var orderId = Guid.NewGuid();
+            var stamp = DateTime.UtcNow;
+            var customerId = "Test";
+
+            var request_client = await harness.ConnectRequestClient<ISubmitOrder>();
+            
+            var response = await request_client.GetResponse<IOrderSubmissionRejected>(new
+            {
+                OrderId = orderId,
+                TimeStamp = stamp,
+                CustomerNumber = customerId
+            });
+
+            Assert.That(consumer.Consumed.Select<ISubmitOrder>().Any(), Is.True);
+            Assert.That(response.Message.OrderId, Is.EqualTo(orderId));
+            Assert.That(harness.Sent.Select<IOrderSubmissionRejected>().Any,Is.True);
 
         }
         finally
