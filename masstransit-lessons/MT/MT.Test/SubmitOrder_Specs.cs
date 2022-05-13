9eac7553ef2f3560f4437347842f4ee44903e288
secvbulrep <bfarukshin@gmail.com>
2022-05-13 18:22:19 +0300

    Добавил тест что паблиш опубкликовал другое событие

--- masstransit-lessons/MT/MT.Test/SubmitOrder_Specs.cs
+++ masstransit-lessons/MT/MT.Test/SubmitOrder_Specs.cs
@@ -70,9 +70,10 @@ public class When_an_order_request_is_consumed
         }
     }
     
+     [Test]
     public async Task Should_response_with_rejected_if_ok2()
     {
-        var harness = new InMemoryTestHarness();
+        var harness = new InMemoryTestHarness{TestTimeout = TimeSpan.FromSeconds(5)};
         var consumer = harness.Consumer<SubmitOrderConsumer>();
 
         await harness.Start();
@@ -95,6 +96,37 @@ public class When_an_order_request_is_consumed
             Assert.That(consumer.Consumed.Select<ISubmitOrder>().Any(), Is.True);
             Assert.That(response.Message.OrderId, Is.EqualTo(orderId));
             Assert.That(harness.Sent.Select<IOrderSubmissionRejected>().Any,Is.True);
+
+        }
+        finally
+        {
+            await harness.Stop();
+        }
+    }
+   
+    
+    [Test]
+    public async Task Should_publish_order_submitted_event()
+    {
+        var harness = new InMemoryTestHarness();
+        var consumer = harness.Consumer<SubmitOrderConsumer>();
+
+        await harness.Start();
+        try
+        {
+
+            var orderId = Guid.NewGuid();
+            var stamp = DateTime.UtcNow;
+            var customerId = "Bob";
+
+            await harness.InputQueueSendEndpoint.Send<ISubmitOrder>(new
+            {
+                OrderId = orderId,
+                TimeStamp = stamp,
+                CustomerNumber = customerId
+            });
+
+            Assert.That(harness.Published.Select<IOrderSubmitted>().Any(), Is.True);
 
         }
         finally
