93afad4c9f79ecf7f66ed6ddd3a61eecbea424c6
SECVBulRep <bfarukshin@gmail.com>
2022-05-15 16:39:25 +0300

    Тестирование ICheckOrder

--- masstransit-lessons/MT/MT.Test/OrderStateMachine_Specs.cs
+++ masstransit-lessons/MT/MT.Test/OrderStateMachine_Specs.cs
@@ -22,7 +22,7 @@ public class Submitting_an_order
             var orderId = Guid.NewGuid();
             var stamp = DateTime.UtcNow;
             var customerId = "Bob";
-            
+
             await harness.Bus.Publish<IOrderSubmitted>(new
             {
                 OrderId = orderId,
@@ -31,19 +31,58 @@ public class Submitting_an_order
             });
 
             Assert.That(saga.Created.Select(x => x.CorrelationId == orderId).Any(), Is.True);
-            
+
             // упадет тест
             //сначала вот это 
             //await Task.Delay(1000);
             // потом вот это 
-           await saga.Exists(orderId, x => x.Submitted);
-            
+            await saga.Exists(orderId, x => x.Submitted);
+
             var instance = saga.Created.Contains(orderId);
-            Assert.That(instance,Is.Not.Null);
-            Assert.That(instance.CurrentState,Is.EqualTo(orderStateMachine.Submitted.Name));
-            Assert.That(instance.CustomerNumber,Is.EqualTo(customerId));
+            Assert.That(instance, Is.Not.Null);
+            Assert.That(instance.CurrentState, Is.EqualTo(orderStateMachine.Submitted.Name));
+            Assert.That(instance.CustomerNumber, Is.EqualTo(customerId));
+        }
+        finally
+        {
+            await harness.Stop();
+        }
+    }
+
+    [Test]
+    public async Task Should_response_to_status_check()
+    {
+        var orderStateMachine = new OrderStateMachine();
+        var harness = new InMemoryTestHarness();
+        var saga = harness.StateMachineSaga<OrderState, OrderStateMachine>(orderStateMachine);
+
+        await harness.Start();
+        try
+        {
+            var orderId = Guid.NewGuid();
+            var stamp = DateTime.UtcNow;
+            var customerId = "Bob";
+
+            await harness.Bus.Publish<IOrderSubmitted>(new
+            {
+                OrderId = orderId,
+                TimeStamp = stamp,
+                CustomerNumber = customerId
+            });
+
+            Assert.That(saga.Created.Select(x => x.CorrelationId == orderId).Any(), Is.True);
+            await saga.Exists(orderId, x => x.Submitted);
+
+            var instance = saga.Created.Contains(orderId);
+            Assert.That(instance, Is.Not.Null);
+            Assert.That(instance.CurrentState, Is.EqualTo(orderStateMachine.Submitted.Name));
+            Assert.That(instance.CustomerNumber, Is.EqualTo(customerId));
+
+            var requestClient = await harness.ConnectRequestClient<ICheckOrder>();
+            var response = await requestClient.GetResponse<IOrderStatus>(new {OrderId = orderId});
             
             
+            Assert.That(response.Message.State,Is.EqualTo(orderStateMachine.Submitted.Name));
         }
         finally
         {
