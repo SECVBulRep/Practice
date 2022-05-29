2f70c6ececbdf7353e62c63f94f7219e2059783f
SECVBulRep <bfarukshin@gmail.com>
2022-05-29 18:15:03 +0300

    * новый сервис для IAllocateInventoryConsumer

--- masstransit-lessons/MT/Warehouse.Service/Program.cs
+++ masstransit-lessons/MT/Warehouse.Service/Program.cs
@@ -1,3 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Warehouse.Components.Consumers;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostingConext, config) => { config.AddJsonFile("appsettings.json", true); })
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.TryAddSingleton(KebabCaseEndpointNameFormatter
+            .Instance); // позже напиши. kebab case лучше чем snake _ kase
+
+        services.AddMassTransit(cfg =>
+        {
+            cfg.AddConsumersFromNamespaceContaining<AllocateInventoryConsumer>();
+
+            cfg.UsingRabbitMq((context, config) =>
+            {
+                config.Host("localhost", "work", h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+                config.ConfigureEndpoints(context);
+            });
+        });
+    })
+    .ConfigureLogging((hostingContext, logging) =>
+    {
+        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        logging.AddConsole();
+    })
+    .Build()
+    .RunAsync();
