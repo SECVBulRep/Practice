fe2a655a4cbd3d88897e23ef8e22d003e36c6735
secvbulrep <bfarukshin@gmail.com>
2022-06-16 15:10:01 +0300

    докомит настроек quartz

---
+++ masstransit-lessons/MT/Warehouse.Service/QuartzConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Specialized;
+
+public class QuartzConfig : Dictionary<string, string>
+{
+    public NameValueCollection ToNameValueCollection()
+    {
+        return this.Aggregate(new NameValueCollection(), (seed, current) =>
+        {
+            seed.Add(current.Key, current.Value);
+            return seed;
+        });
+    }
+}
