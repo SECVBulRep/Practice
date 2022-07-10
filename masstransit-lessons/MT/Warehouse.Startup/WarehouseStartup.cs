411bf1d7059138310c5df020bb5fe543b027337c
SECVBulRep <bfarukshin@gmail.com>
2022-07-10 20:10:54 +0300

    fix

---
+++ masstransit-lessons/MT/Warehouse.Startup/WarehouseStartup.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using MassTransit.ExtensionsDependencyInjectionIntegration;
+using MassTransit.ExtensionsDependencyInjectionIntegration.MultiBus;
+using MassTransit.Platform.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Warehouse.Components.Consumers;
+using Warehouse.Components.StateMachines;
+
+namespace Warehouse.Startup;
+
+public class WarehouseStartup: IPlatformStartup
+{
+
+    public void ConfigureMassTransit(IServiceCollectionBusConfigurator configurator, IServiceCollection services)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ConfigureBus<TEndpointConfigurator>(IBusFactoryConfigurator<TEndpointConfigurator> configurator, IBusRegistrationContext context) where TEndpointConfigurator : IReceiveEndpointConfigurator
+    {
+        throw new NotImplementedException();
+    }
+}
