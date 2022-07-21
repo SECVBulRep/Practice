aec438fb0b34cb5c7a6380572393db15b6f5c6d8
SECVBulRep <bfarukshin@gmail.com>
2022-07-21 21:18:07 +0300

    * базовые настройки  кролика

--- masstransit-lessons/Rabbit.Details/Rabbit.Details/Rabbit.Details/Program.cs
+++ masstransit-lessons/Rabbit.Details/Rabbit.Details/Rabbit.Details/Program.cs
@@ -1,9 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using MassTransit;
+
 internal class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("hi");
+        var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+        {
+            cfg.Host("localhost", h =>
+            {
+                h.Username("guest");
+                h.Password("guest");
+            });
+        });
+
+
+        var cancelationToken = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+        
+        // для задержки запуска 
+        await busControl.StartAsync(cancelationToken.Token);
+
+        try
+        {
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            await busControl.StopAsync(CancellationToken.None);
+        }
     }
 }
