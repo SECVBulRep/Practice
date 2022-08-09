d4684fc0f9c0437f7d92293d06e06e0013cc02db
SECVBulRep <bfarukshin@gmail.com>
2022-08-09 13:40:16 +0300

    * пример прерывания пайплайна

--- masstransit-lessons/MT/Delivery.Components/Filters/ConsoleConsumeFilter.cs
+++ masstransit-lessons/MT/Delivery.Components/Filters/ConsoleConsumeFilter.cs
@@ -4,31 +4,16 @@ namespace Delivery.Components.Filters;
 
 public class ConsoleConsumeFilter : IFilter<ConsumeContext>
 {
-    public async Task Send(ConsumeContext context, IPipe<ConsumeContext> next)
+    public  Task Send(ConsumeContext context, IPipe<ConsumeContext> next)
     {
         Console.WriteLine($"Consume: {context.MessageId}");
-        await next.Send(context);
+        //await next.Send(context);
+        return Task.CompletedTask;
     }
 
     public void Probe(ProbeContext context)
     {
         context.CreateScope("ConsoleConsumeFilter");
-        context.Add("output", "console");
-    }
-}
-
-
-public class ConsoleConsumeWithConsumerFilter<TConsumer> : IFilter<ConsumerConsumeContext<TConsumer>> where TConsumer : class
-{
-    public async Task Send(ConsumerConsumeContext<TConsumer> context, IPipe<ConsumerConsumeContext<TConsumer>> next)
-    {
-        Console.WriteLine($"Consume With Consumer : {context.MessageId}");
-        await next.Send(context);
-    }
-
-    public void Probe(ProbeContext context)
-    {
-        context.CreateScope("ConsoleWithConsumerConsumeFilter");
         context.Add("output", "console");
     }
 }
---
+++ masstransit-lessons/MT/Delivery.Components/Filters/ConsoleConsumeWithConsumerFilter.cs
@@ -0,0 +1,18 @@
+﻿using MassTransit;
+
+namespace Delivery.Components.Filters;
+
+public class ConsoleConsumeWithConsumerFilter<TConsumer> : IFilter<ConsumerConsumeContext<TConsumer>> where TConsumer : class
+{
+    public async Task Send(ConsumerConsumeContext<TConsumer> context, IPipe<ConsumerConsumeContext<TConsumer>> next)
+    {
+        Console.WriteLine($"Consume With Consumer : {context.MessageId}");
+        await next.Send(context);
+    }
+
+    public void Probe(ProbeContext context)
+    {
+        context.CreateScope("ConsoleWithConsumerConsumeFilter");
+        context.Add("output", "console");
+    }
+}
