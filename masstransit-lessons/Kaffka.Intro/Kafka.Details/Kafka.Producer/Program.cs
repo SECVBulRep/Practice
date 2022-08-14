5d0c3c6c9e5f58d0a11f1ab385f6cdd9a3f54ba8
SECVBulRep <bfarukshin@gmail.com>
2022-08-14 17:50:16 +0300

    * пример продюсера кафки

---
+++ masstransit-lessons/Kaffka.Intro/Kafka.Details/Kafka.Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092",
+};
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    var response = producer.ProduceAsync("weather-topic", new Message<Null, string> { Value ="Hello Kafka!!"});
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+Console.WriteLine("Hello, World!");
+
+
