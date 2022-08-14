be415ebec6d3868c6e38a35d7c2e3b0949af3809
SECVBulRep <bfarukshin@gmail.com>
2022-08-14 18:10:27 +0300

    * пример консюмера

--- masstransit-lessons/Kaffka.Intro/Kafka.Details/Kafka.Producer/Program.cs
+++ masstransit-lessons/Kaffka.Intro/Kafka.Details/Kafka.Producer/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Confluent.Kafka;
+using Newtonsoft.Json;
 
 var config = new ProducerConfig
 {
@@ -11,13 +12,20 @@ using var producer = new ProducerBuilder<Null, string>(config).Build();
 
 try
 {
-    var response = producer.ProduceAsync("weather-topic", new Message<Null, string> { Value ="Hello Kafka!!"});
+    string? state;
+    while ((state = Console.ReadLine()) != null)
+    {
+        var response = producer.ProduceAsync("weather-topic",
+            new Message<Null, string> {Value = JsonConvert.SerializeObject(new Weather(state, 70))});
+    }
+}
+catch (ProduceException<Null, string> exc)
+{
+    Console.WriteLine(exc);
 }
 catch (Exception e)
 {
     Console.WriteLine(e);
 }
 
-Console.WriteLine("Hello, World!");
-
-
+public record Weather(string State, int Temperature);
