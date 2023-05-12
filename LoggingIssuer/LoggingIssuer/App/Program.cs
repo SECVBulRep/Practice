b7e8dff95f4b1694ca0430138cfb5e36fb8fe02e
Bulat <farukshin@molot.ru>
2023-05-12 11:10:22 +0300

    * пример логирования

---
+++ LoggingIssuer/LoggingIssuer/App/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Logging;
+
+namespace App;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .SetMinimumLevel(LogLevel.Warning)
+                .AddConsole();
+        });
+      
+        ILogger logger = loggerFactory.CreateLogger<Program>();
+        logger.LogTrace("Info Trace");
+        logger.LogDebug("Info Debug");
+        logger.LogInformation("Info Log");
+        logger.LogWarning("Warning Log");
+        logger.LogError("Error Log");
+        logger.LogCritical("Critical Log");
+        
+        Console.ReadLine();
+    }
+}
