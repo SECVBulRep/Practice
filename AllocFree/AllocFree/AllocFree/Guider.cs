c15252a8f7c918a64609a2e01bdd47d1fdecdc0a
Bulat <farukshin@molot.ru>
2023-05-17 10:23:58 +0300

    * загатовка под allocfree

---
+++ AllocFree/AllocFree/AllocFree/Guider.cs
@@ -0,0 +1,22 @@
+﻿namespace AllocFree;
+
+public class Guider
+{
+    public static string ToStringFromGuid(Guid id)
+    {
+        return Convert.ToBase64String(id.ToByteArray())
+            .Replace("/", "-")
+            .Replace("+", "_")
+            .Replace("=", string.Empty);
+    }
+
+    public static Guid ToGuidFromString(string id)
+    {
+        byte[] base64 = Convert.FromBase64String(
+            id
+                .Replace("-", "/")
+                .Replace("_", "+")
+            + "==");
+        return new Guid(base64);
+    }
+}
---
+++ AllocFree/AllocFree/AllocFree/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AllocFree;
+
+var id = Guid.NewGuid();
+Console.WriteLine(id);
+
+var base64Id = Convert.ToBase64String(id.ToByteArray());
+Console.WriteLine(base64Id);
+
+var urlFriendlybase64Id = Guider.ToStringFromGuid(id);
+Console.WriteLine(urlFriendlybase64Id);
+
+var idAgain = Guider.ToGuidFromString(urlFriendlybase64Id);
+Console.WriteLine(idAgain);
+
+
+
