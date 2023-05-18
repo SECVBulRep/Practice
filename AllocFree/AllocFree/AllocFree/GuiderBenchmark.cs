a2f07899948f4686e38a28e9d190ce6af073f037
Bulat <farukshin@molot.ru>
2023-05-18 10:21:51 +0300

    * добавил бенчмарк

---
+++ AllocFree/AllocFree/AllocFree/GuiderBenchmark.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AllocFree;
+
+
+[MemoryDiagnoser(false)]
+public class GuiderBenchmark
+{
+    private static readonly Guid TestIdAsGuid = Guid.Parse("ebc40f68-d173-43d3-911b-6b2d17513087");
+    private static readonly string TestIdAsString = "aA-E63PR00ORG2stF1Ewhw";
+
+    [Benchmark()]
+    public Guid ToGuidFromString()
+    {
+        return Guider.ToGuidFromString(TestIdAsString);
+    }
+    
+    [Benchmark()]
+    public string  FromGuidToString()
+    {
+        return Guider.ToStringFromGuid(TestIdAsGuid);
+    }
+}
--- AllocFree/AllocFree/AllocFree/Program.cs
+++ AllocFree/AllocFree/AllocFree/Program.cs
@@ -1,18 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using AllocFree;
+using BenchmarkDotNet.Running;
 
-var id = Guid.NewGuid();
-Console.WriteLine(id);
+// var id = Guid.NewGuid();
+// Console.WriteLine(id);
+//
+// var base64Id = Convert.ToBase64String(id.ToByteArray());
+// Console.WriteLine(base64Id);
+//
+// var urlFriendlybase64Id = Guider.ToStringFromGuid(id);
+// Console.WriteLine(urlFriendlybase64Id);
+//
+// var idAgain = Guider.ToGuidFromString(urlFriendlybase64Id);
+// Console.WriteLine(idAgain);
+//
+//
 
-var base64Id = Convert.ToBase64String(id.ToByteArray());
-Console.WriteLine(base64Id);
-
-var urlFriendlybase64Id = Guider.ToStringFromGuid(id);
-Console.WriteLine(urlFriendlybase64Id);
-
-var idAgain = Guider.ToGuidFromString(urlFriendlybase64Id);
-Console.WriteLine(idAgain);
-
-
-
+BenchmarkRunner.Run<GuiderBenchmark>();
