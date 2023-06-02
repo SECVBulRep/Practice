4e36933cc54211a2f9866262b9d30706f434f636
Bulat <farukshin@molot.ru>
2023-06-02 11:58:06 +0300

    * поиск контайн

--- FrozenList/F/F.App/SetBenchmark.cs
+++ FrozenList/F/F.App/SetBenchmark.cs
@@ -5,6 +5,8 @@ using BenchmarkDotNet.Attributes;
 namespace F.App;
 
 
+
+
 [MemoryDiagnoser()]
 public class SetBenchmark
 {
@@ -16,17 +18,17 @@ public class SetBenchmark
     private ImmutableHashSet<int> _immutableHashSet = null!;
     private FrozenSet<int> _frozenSet = null!;
 
-    private int _middle;
+    private int _searchItem;
 
-    [Params(1000)]
+    [Params(10000)]
     public int Size { get; set; }
 
     [GlobalSetup]
     public void Setup()
     {
-        _random = new Random(66);
+        _random = new Random(420);
         _list = Enumerable.Range(0, Size).Select(_ => _random.Next()).ToList();
-        _middle = _list[Size / 2];
+        _searchItem = _list[Size / 2];
 
         _hashSet = _list.ToHashSet();
         _immutableHashSet = _list.ToImmutableHashSet();
@@ -36,10 +38,20 @@ public class SetBenchmark
 
     [Benchmark()]
     public HashSet<int> HashSet_Init() => _list.ToHashSet();
-
+    
     [Benchmark()]
     public ImmutableHashSet<int> ImmutableHashSet_Init() => _list.ToImmutableHashSet();
     
     [Benchmark()]
     public FrozenSet<int> FrozenSet_Init() => _list.ToFrozenSet();
+    
+    [Benchmark()]
+    public bool HashSet_Contains() => _hashSet.Contains(_searchItem);
+
+    [Benchmark()]
+    public bool ImmutableHashSet_Contains() => _immutableHashSet.Contains(_searchItem);
+    
+    [Benchmark()]
+    public bool FrozenSet_Contains() => _frozenSet.Contains(_searchItem);
+    
 }
