be67a6ecec33c39bc55fddee4d990be263ebc00f
Bulat <farukshin@molot.ru>
2023-06-02 11:17:42 +0300

    * Создал  бенчмарк  добавил инит бенчинг

--- FrozenList/F/F.App/Program.cs
+++ FrozenList/F/F.App/Program.cs
@@ -1,22 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections.Frozen;
 using System.Collections.Immutable;
+using BenchmarkDotNet.Running;
+using F.App;
 
-var list  = new List<int>{1,2,2,3};
+// var list  = new List<int>{1,2,2,3};
+//
+// // list.Add();
+// // list.Remove() и тд и тп;
+//
+// var hashSet = list.ToHashSet();
+//
+// // hashSet.Add()
+// // hashSet.Remove() и тд и тп; НЕ Потокобезопасный 
+//
+// var immutableHashSet = list.ToImmutableHashSet();
+//
+// // ImmutablehashSet.Add()
+// // ImmutablehashSet.Remove() и тд и тп;  Потокобезопасный  но каждый раз создает копию / РАсскажи что это сбалансированное дереов и что с ростом данных add/remove все больше и больше будет страдать
+//
+//
+// var frozenSet = list.ToFrozenSet();
+// // нет add/delete и тд и тп !!! Ну какой trade of ???
+//
+//
+// Console.WriteLine("Hello, World!");
 
-// list.Add();
-// list.Remove() и тд и тп;
-
-var hashSet = list.ToHashSet();
-
-// hashSet.Add()
-// hashSet.Remove() и тд и тп; НЕ Потокобезопасный 
-
-var immutableHashSet = list.ToImmutableHashSet();
-
-// ImmutablehashSet.Add()
-// ImmutablehashSet.Remove() и тд и тп;  Потокобезопасный  но каждый раз создает копию / РАсскажи что это сбалансированное дереов и что с ростом данных add/remove все больше и больше будет страдать
-
-
-
-Console.WriteLine("Hello, World!");
+BenchmarkRunner.Run<SetBenchmark>();
---
+++ FrozenList/F/F.App/SetBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Frozen;
+using System.Collections.Immutable;
+using BenchmarkDotNet.Attributes;
+
+namespace F.App;
+
+
+[MemoryDiagnoser()]
+public class SetBenchmark
+{
+    private Random _random = null!;
+
+    private List<int> _list = null!;
+
+    private HashSet<int> _hashSet = null!;
+    private ImmutableHashSet<int> _immutableHashSet = null!;
+    private FrozenSet<int> _frozenSet = null!;
+
+    private int _middle;
+
+    [Params(1000)]
+    public int Size { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _random = new Random(66);
+        _list = Enumerable.Range(0, Size).Select(_ => _random.Next()).ToList();
+        _middle = _list[Size / 2];
+
+        _hashSet = _list.ToHashSet();
+        _immutableHashSet = _list.ToImmutableHashSet();
+        _frozenSet = _list.ToFrozenSet();
+
+    }
+
+    [Benchmark()]
+    public HashSet<int> HashSet_Init() => _list.ToHashSet();
+
+    [Benchmark()]
+    public ImmutableHashSet<int> ImmutableHashSet_Init() => _list.ToImmutableHashSet();
+    
+    [Benchmark()]
+    public FrozenSet<int> FrozenSet_Init() => _list.ToFrozenSet();
+}
