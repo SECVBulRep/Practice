8a66b294bfb63fc64a0865560a00890112839a46
bulat <farukshin@molot.ru>
2023-07-14 12:19:02 +0300

    *  понизил версию до 7 * убрал ToList() Чтобы не грузилась вся таблица

--- OptimizeMe/OptimizeMe/OptimizeMe.App/AntiVirusFriendlyConfig.cs
+++ OptimizeMe/OptimizeMe/OptimizeMe.App/AntiVirusFriendlyConfig.cs
@@ -1,10 +1,17 @@
-﻿using BenchmarkDotNet.Configs;
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using Bogus;
 
 namespace OptimizeMe.App;
 
 public class AntiVirusFriendlyConfig : ManualConfig
 {
-    public AntiVirusFriendlyConfig() => this.AddJob(Job.MediumRun.WithToolchain(InProcessNoEmitToolchain.Instance));
+    public AntiVirusFriendlyConfig()
+    {
+        SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default.WithRatioStyle(RatioStyle.Percentage);
+        
+        this.AddJob(Job.MediumRun.WithToolchain(InProcessNoEmitToolchain.Instance));
+    }
 }
--- OptimizeMe/OptimizeMe/OptimizeMe.App/Benchmarks.cs
+++ OptimizeMe/OptimizeMe/OptimizeMe.App/Benchmarks.cs
@@ -9,7 +9,7 @@ namespace OptimizeMe.App;
 
 [Config(typeof(AntiVirusFriendlyConfig))]
 [MemoryDiagnoser]
-[HideColumns("Job", "RatioSD", "StdDev", "Alloc Ratio")]
+[HideColumns("Job",  "StdDev")]
 public class Benchmarks
 {
     private AppDbContext _appDbContext;
@@ -26,7 +26,7 @@ public class Benchmarks
         await _generator.Generate(10, _appDbContext);
     }
 
-    [Benchmark(26, "D:\\WORK\\Practice\\Practice\\OptimizeMe\\OptimizeMe\\OptimizeMe.App\\Benchmarks.cs")]
+    [Benchmark(Baseline = true)]
     public List<AuthorDTO> GetAuthors()
     {
         using (AppDbContext appDbContext = new AppDbContext())
@@ -88,6 +88,68 @@ public class Benchmarks
         }
     }
 
+    
+      [Benchmark]
+    public List<AuthorDTO> GetAuthorsOpt()
+    {
+        using (AppDbContext appDbContext = new AppDbContext())
+        {
+            List<AuthorDTO> list =
+                appDbContext.Authors
+                    .Include<Author, User>(x => x.User)
+                    .ThenInclude(x => x.UserRoles)
+                    .ThenInclude(x => x.Role)
+                    .Include(x => x.Books)
+                    .ThenInclude(x => x.Publisher)
+                    .Select(x => new AuthorDTO
+                    {
+                        UserCreated = x.User.Created,
+                        UserEmailConfirmed = x.User.EmailConfirmed,
+                        UserFirstName = x.User.FirstName,
+                        UserLastActivity = x.User.LastActivity,
+                        UserLastName = x.User.LastName,
+                        UserEmail = x.User.Email,
+                        UserName = x.User.UserName,
+                        UserId = x.User.Id,
+                        RoleId = x.User.UserRoles
+                            .FirstOrDefault(y => y.UserId == x.UserId).RoleId,
+                        BooksCount = x.BooksCount,
+                        AllBooks = x.Books.Select(y => new BookDto
+                        {
+                            Id = y.Id,
+                            Name = y.Name,
+                            Published = y.Published,
+                            ISBN = y.ISBN,
+                            PublisherName = y.Publisher.Name
+                        }).ToList(),
+                        AuthorAge = x.Age,
+                        AuthorCountry = x.Country,
+                        AuthorNickName = x.NickName,
+                        Id = x.Id
+                    })
+                    .Where(x => x.AuthorCountry == "Serbia" && x.AuthorAge > 26)
+                    .ToList().OrderByDescending(x => x.BooksCount)
+                    .ToList().Take(2).ToList();
+            List<AuthorDTO> authors = new List<AuthorDTO>();
+            foreach (AuthorDTO authorDto in list)
+            {
+                List<BookDto> bookDtoList = new List<BookDto>();
+                foreach (BookDto allBook in authorDto.AllBooks)
+                {
+                    if (allBook.Published.Year < 2022)
+                    {
+                        allBook.PublishedYear = allBook.Published.Year;
+                        bookDtoList.Add(allBook);
+                    }
+                }
+
+                authorDto.AllBooks = bookDtoList;
+                authors.Add(authorDto);
+            }
+
+            return authors;
+        }
+    }
     [GlobalCleanup]
     public async Task CleanUp()
     {
