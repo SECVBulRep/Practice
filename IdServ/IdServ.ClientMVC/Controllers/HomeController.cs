5c5b6b17c7555dbad3292baaa83e9a1266105a0b
bulat <farukshin@molot.ru>
2023-09-20 16:14:48 +0300

    * сделал просмотр токенов в вью

--- IdServ/IdServ.ClientMVC/Controllers/HomeController.cs
+++ IdServ/IdServ.ClientMVC/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using IdServ.ClientMVC.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 
 namespace IdServ.ClientMVC.Controllers;
@@ -20,9 +23,18 @@ public class HomeController : Controller
     }
 
     [Authorize]
-    public IActionResult Privacy()
+    public async Task<IActionResult> Privacy()
     {
-        return View();
+        var id_token = await HttpContext.GetTokenAsync("id_token");
+        var access_token = await HttpContext.GetTokenAsync("access_token");
+      
+        var handler = new JwtSecurityTokenHandler();
+        
+        var userInfo = new AuthInfo();
+        userInfo.IdToken =  handler.ReadJwtToken(id_token);
+        userInfo.AccessToken = handler.ReadJwtToken(access_token);
+        userInfo.UserInfo = User.Claims;
+        return View(userInfo);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -30,4 +42,13 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+}
+
+public class AuthInfo
+{
+    public JwtSecurityToken IdToken { get; set; }
+
+    public JwtSecurityToken AccessToken { get; set; }
+
+    public IEnumerable<Claim> UserInfo { get; set; }
 }
