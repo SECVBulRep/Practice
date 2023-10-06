2e70787affa0b17dade96b55fa24702a3c3675de
bulat <farukshin@molot.ru>
2023-10-06 15:56:38 +0300

    *  пустой проект для js

---
+++ IdServ/IdServ.ClientJava/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IdServ.ClientJava.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
